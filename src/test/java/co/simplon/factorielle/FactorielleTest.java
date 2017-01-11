a28114863ef748fde1b7ad11f3e644802e0803b5
Laurent <laurent.simplon@gmail.com>
2017-01-11 09:33:56 +0100

    exercice 1 - exception

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -94,5 +94,14 @@ public class FactorielleTest {
 		assertThat(resultat, equalTo(resultatAttendu));
 		assertThat(resultat == resultatAttendu, is(true));
 	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	
+	public void factorielle_de_20_renvoie_UnsupportedOperationException(){
+		long entier=50;
+			
+		factorielle.calculer(entier);
+				
+	}
 
 }
