0b5aff3051871d2f993c34e5f8a0e5b795101370
Laurent <laurent.simplon@gmail.com>
2017-01-11 14:08:48 +0100

    encore un essai...

---
+++ src/test/java/co/simplon/factorielle/Factorielleunvailableexception.java
@@ -0,0 +1,9 @@
+package co.simplon.factorielle;
+
+public  class Factorielleunvailableexception  extends Exception {
+	
+	public Factorielleunvailableexception(String message) {
+		super("ATTENTION NOMBRE NEGATIF");
+	}
+
+}
