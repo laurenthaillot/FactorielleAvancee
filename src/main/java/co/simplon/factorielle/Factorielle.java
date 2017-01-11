98e3b1cff4fd5e62384e3fc8e8db3fb9726bc7bb
Laurent <laurent.simplon@gmail.com>
2017-01-11 10:53:46 +0100

    modif exercice 1 sur exception

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -3,19 +3,35 @@ package co.simplon.factorielle;
 /**
  * Factorielle
  */
-public class Factorielle 
-{
-    public Factorielle() {
-    		super();
-    }
-    
-    public long calculer(long n) {
-    		if (n > 1) {
-    			return n * calculer(n-1);
-    		} else if (n < 0) {
-    			throw new IllegalArgumentException();
-    		}
-    		return 1;
-    }
+public class Factorielle {
+	public Factorielle() {
+		super();
+	}
+
+	public long calculer(long n) {
+
+		if (n > 1) {
+			long fact = calculer(n - 1);
+			long factn = n * fact;
+
+			if (factn <= fact) {
+				throw new UnsupportedOperationException();
+			}
+			return factn;
+		}
+
+		else if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		return 1;
+
+		/*
+		 * if (n > 1) { return n * calculer(-1); }
+		 * 
+		 * else if (n < 0) {
+		 * 
+		 * throw newIllegalArgumentException(); } return 1;
+		 */
+	}
 
 }
