4bd858c1e9987ae032723dc05a4e3f63c25955f2
Laurent <laurent.simplon@gmail.com>
2017-01-12 17:12:11 +0100

    exercices 4 - envoie n°3

--- src/main/java/co/simplon/factorielle/Main.java
+++ src/main/java/co/simplon/factorielle/Main.java
@@ -7,21 +7,25 @@ public class Main {
 	public static void main(String[] args) {
 		long a = 0;
 		long resultat;
-		String str = args[0];
-
+		
 		try {
-			a = Long.parseLong(str);
+			a = Long.parseLong(args[0]);
 		} catch (NumberFormatException nfe) {
-			System.out.println("Necessite une valeur numérique (Main)");
+			System.out.println("Cas Erreur 1 : Necessite une valeur numérique. Votre valeur est : "+ args[0]);
 			System.exit(10);
+		} catch (ArrayIndexOutOfBoundsException AIOBE) {
+			System.out.println("Cas erreur 4 : La valeur est vide");
+			System.exit(5);
 		}
+		
 		try {
 			resultat = Factorielle.calculer(a);
-			System.out.println("Bravo ! Le resultat est : " + resultat);
+			System.out.println("Bravo ! La factorielle de " + a + " est : " + resultat);
 		} catch (IllegalArgumentException IAE) {
-			System.out.println("Attention nombre négatif");
+			System.out.println("Cas Erreur 2 : Attention nombre négatif. Votre valeur est : "+ a);
 		} catch (Factorielleunvailableexception FUE) {
-			System.out.println("Resultat incohérent");
-		}
+			System.out.println("Cas Erreur 3 : " + a + " est une valeur incoherente pour ce type de calcul");
+		} 
 	}
 }
+
